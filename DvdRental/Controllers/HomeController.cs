7a1ca4261488d3cf6b33c558237bb5fde0ba5302
Daryl Johnston <daryl874@gmail.com>
2014-10-10 23:19:08 +0100

    init commit
    initial commit

---
+++ DvdRental/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using DvdRental.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DvdRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+
+            var vm = new IndexModel();
+            return View(vm);
+        }
+
+        public ActionResult Info() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel vm) {
+
+
+            ViewBag.Result = String.Format("{0} {1};",vm.UserName, vm.Password);
+            
+            return View();
+        
+        }
+
+    }
+}
